7b3e3dd4193bd20adef99559b3c37b009b41931e
Alejandro Garrido Mota <garridomota@gmail.com>
2020-06-12 09:59:02 +0100

    Improved structure bootstrap-new-aws-account

--- terraform/aws-accounts/cloud-platform-ephemeral-test/global-resources/main.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/global-resources/main.tf
@@ -8,3 +8,39 @@ terraform {
     encrypt        = true
   }
 }
+
+###########################
+# Security Baseguidelines #
+###########################
+
+module "baselines" {
+  #source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-iam?ref=0.0.1"
+  source = "/Users/mogaal/workspace/github/ministryofjustice/cloud-platform-terraform-awsaccounts-baselines"
+
+  account_name  = "cloud-platform-ephemeral-test"
+  region        = "eu-west-2"
+  slack_webhook = var.baselines_alerts_slack_webhook
+  slack_channel = var.baselines_alerts_slack_channel
+}
+
+#######
+# IAM #
+#######
+
+module "iam" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-iam?ref=0.0.1"
+
+  aws_account_name = "cloud-platform-ephemeral-test"
+}
+
+#######
+# DNS #
+#######
+
+# Nothing yet
+
+# new parent DNS zone for clusters
+resource "aws_route53_zone" "root_aws_account_cloudplatform_justice_gov_uk" {
+  name     = "ephemeral-test.cloud-platform.service.justice.gov.uk."
+}
+
--- terraform/aws-accounts/cloud-platform-ephemeral-test/global-resources/route53.tf
+++
@@ -1,6 +0,0 @@
-
-# new parent DNS zone for clusters
-resource "aws_route53_zone" "root_aws_account_cloudplatform_justice_gov_uk" {
-  name     = "ephemeral-test.cloud-platform.service.justice.gov.uk."
-}
-
